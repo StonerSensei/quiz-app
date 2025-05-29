402b4c3ce4aa0143d88992cf1b4d96f53ef6ac81
Parth Tiwari <parthtiwari321@gmail.com>
2025-05-29 15:34:34 +0530

    Setup env config and hide secrets

--- quiz-backend/src/main/java/com/parth/quizapp/QuizappApplication.java
+++ quiz-backend/src/main/java/com/parth/quizapp/QuizappApplication.java
@@ -1,5 +1,6 @@
 package com.parth.quizapp;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuizappApplication {
 
 	public static void main(String[] args) {
+		// Load .env and inject variables into system properties
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+		// Start Spring Boot app
 		SpringApplication.run(QuizappApplication.class, args);
 	}
-
 }
